bdc319a8d76e62d9c7dfa8a73eb7b42f7ee8248a
Chris Kechagias <ck.chris.kechagias@gmail.com>
2026-03-02 00:21:13 +0200

    add home router

---
+++ home.py
@@ -0,0 +1,17 @@
+# Third-Party Imports
+from fastapi import APIRouter
+
+# Local/First-Party Imports
+from config import config
+
+router = APIRouter()
+
+
+@router.get("/")
+def read_root():
+    return {
+        "message": config.app_name,
+        "version": config.version,
+        "docs": "/docs",
+        "endpoints": "/products",
+    }
