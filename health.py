bfeef580aaf803545e59dc31cde1946eb2464566
Chris Kechagias <ck.chris.kechagias@gmail.com>
2026-03-02 00:18:02 +0200

    proper format and lint for health.py

--- health.py
+++ health.py
@@ -10,11 +10,10 @@ from models import HealthResponse
 
 router = APIRouter()
 
+
 @router.head("/health")
 @router.get("/health", response_model=HealthResponse, tags=["System"])
 def health_check():
     return HealthResponse(
-        status="ok",
-        version=config.version,
-        uptime=time.time() - config.start_time
+        status="ok", version=config.version, uptime=time.time() - config.start_time
     )
